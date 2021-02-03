2a3ed131625870077aee6e96bcc06c39e55a7eba
CHH <chh2126@gmail.com>
2021-02-03 09:11:48 +0900

    source tree test

--- myshop/urls.py
+++ myshop/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('accounts/', include('django.contrib.auth.urls')), 
+    
     path('accounts/register/', UserCreateView.as_view(), name='register'),
     path('accounts/register/done/', UserCreateDoneTV.as_view(), name='register_done'),
 
